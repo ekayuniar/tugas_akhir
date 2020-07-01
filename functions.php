340c42c8a261e8d963d4c26fa6c1ae85678fad1c
eka yuniar <sayayuniar@gmail.com>
2020-07-01 11:36:57 +0700

    tambah searching

--- functions.php
+++ functions.php
@@ -38,3 +38,40 @@ function hapus($id)
     mysqli_query($koneksi, "DELETE FROM data_mhs WHERE id = $id");
     return mysqli_affected_rows($koneksi);
 }
+
+function ubah($data)
+{
+    global $koneksi;
+    $id = $data["id"];
+    $nama = htmlspecialchars($data["nama"]);
+    $nim = htmlspecialchars($data["nim"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambar = htmlspecialchars($data["gambar"]);
+
+    // query insert ke database
+    $query = "UPDATE data_mhs SET
+                nama = '$nama',
+                nim = '$nim',
+                email = '$email',
+                jurusan = '$jurusan',
+                gambar = '$gambar'
+            WHERE id = $id
+    ";
+
+    mysqli_query($koneksi, $query);
+    return mysqli_affected_rows($koneksi);
+}
+
+function cari($keyword)
+{
+    $query = "SELECT * FROM data_mhs
+                WHERE
+                nama LIKE '%$keyword%' OR
+                nim LIKE '%$keyword%' OR
+                email LIKE '%$keyword%' OR
+                jurusan LIKE '%$keyword%'
+    ";
+
+    return query($query);
+}
--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 require 'functions.php';
 $mahasiswa = query("SELECT * FROM data_mhs");
+
+// tombol cari ditekan?
+if (isset($_POST["cari"])) {
+    $mahasiswa = cari($_POST["keyword"]);
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -16,6 +23,13 @@ $mahasiswa = query("SELECT * FROM data_mhs");
     <h1>Daftar Mahasiswa</h1>
     <a href="tambah.php">Tambah Data Mahasiswa</a>
     <br></br>
+
+    <form action="" method="POST">
+        <input type="text" name="keyword" size="30" autofocus placeholder="Masukkan kata kunci pencarian"
+            autocomplete="off">
+        <button type="submit" name="cari">Cari</button>
+    </form>
+    <br>
 
     <table border="1" cellpadding="10" cellspacing="0">
         <tr>
@@ -33,7 +47,7 @@ $mahasiswa = query("SELECT * FROM data_mhs");
         <tr>
             <td><?= $i ?></td>
             <td>
-                <a href="">ubah</a> |
+                <a href="ubah.php?id=<?= $row['id']; ?>">ubah</a> |
                 <a href="hapus.php?id=<?= $row['id']; ?>">hapus</a>
             </td>
             <td>
